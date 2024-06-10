474b8f9e1d2a9ffef218f7713dc35a69d4300510
wuailingying <1131977686@qq.com>
2024-06-10 10:08:29 +0800

    Initial version of jytest generated by generator-jhipster@8.4.0

---
+++ src/test/java/com/cvicse/IntegrationTest.java
@@ -0,0 +1,20 @@
+package com.cvicse;
+
+import com.cvicse.config.AsyncSyncConfiguration;
+import com.cvicse.config.EmbeddedSQL;
+import com.cvicse.config.JacksonConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { JytestApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+public @interface IntegrationTest {
+}
