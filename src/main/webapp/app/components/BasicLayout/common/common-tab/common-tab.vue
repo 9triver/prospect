46c86070573774cd725a2c4387c09463343de206
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-08-15 16:17:58 +0800

    多页签展示图标

--- src/main/webapp/app/components/BasicLayout/common/common-tab/common-tab.vue
+++ src/main/webapp/app/components/BasicLayout/common/common-tab/common-tab.vue
@@ -6,46 +6,58 @@
           @tab-remove="deleteMenu"
           @tab-change="undateMenu"
         >
-          <el-tab-pane key="home" label="主页" name="home"  />
+          <el-tab-pane key="home" label="主页" name="home">
+            <template #label>
+              <span class="custom-tabs-label">
+                <el-icon><HomeFilled/></el-icon>主页
+              </span>
+            </template>
+          </el-tab-pane>
           <el-tab-pane
             v-for="item in editableTabs"
             :key="item.name"
-            :label="calcName(item.title)"
+            :label="item.title"
             :name="item.name"
             closable
-          />
+          >
+            <template #label>
+              <span class="custom-tabs-label">
+                <el-icon><component :is="item.icon"/></el-icon>{{item.title}}
+              </span>
+            </template>
+          </el-tab-pane>
         </el-tabs>
     </div>
   </template>
   
   <script lang="ts" setup>
-  import { ref,computed} from 'vue'
+  import { computed} from 'vue'
   import useMenuTabStore from '@/store/model/menuTabs/index'
-  import {useRouter} from 'vue-router'
   import {storeToRefs} from 'pinia'
   import { useI18n } from 'vue-i18n';
 
   let menuTabStore = useMenuTabStore()
   const {menuTab} = storeToRefs(menuTabStore)
-  const {addMenu,deleteMenu,undateMenu} = menuTabStore
+  const {deleteMenu,undateMenu} = menuTabStore
 
 
-  const router = useRouter()
-  router.afterEach(to=>{
-    const {name,matched,path} = to
-    console.log("zhou",to)
-    if(matched[1]){
-        addMenu({name:String(name),path})
-    }
-  })
+  // const router = useRouter()
+  // router.afterEach(to=>{
+  //   const {name,matched,path} = to
+  //   console.log("zhou",to)
+  //   if(matched.length>1){
+  //       addMenu({name:String(name),path})
+  //   }else{
+  //     if(matched.length==1&&matched[0].name!="NotFound"){
+  //       addMenu({name:String(name),path})
+  //     }
+  //   }
+  // })
 
 
   const editableTabs = computed(()=>{
     return menuTab.value.openMenus.map(menu=>{
-        return{
-        title: menu.name,
-        name: menu.name
-        }
+        return menu
     })
   }) 
 
@@ -79,11 +91,25 @@
   
   </script>
   
-  <style scoped>
+  <style lang="scss">
     .common-tab-wrapper{
       background: #fff;
       margin: 10px -10px 0px;
       border-radius: 10px;
+      .custom-tabs-label {
+        display: flex;
+        align-items: center;
+      }
+      .el-tabs__item.is-top.is-active{
+        i{
+          color: #011c4a !important;
+        }
+        .custom-tabs-label {
+          color: #011c4a !important;
+          font-weight: bold;
+          font-size: 16px;
+        }
+      }
     }
     .el-tabs__new-tab{
         display: none;
