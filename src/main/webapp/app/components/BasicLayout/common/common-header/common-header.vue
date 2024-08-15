6eb964d24ef1a46cac0c413af8704485e558a60c
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-08-15 17:31:20 +0800

    右上角样式修改

--- src/main/webapp/app/components/BasicLayout/common/common-header/common-header.vue
+++ src/main/webapp/app/components/BasicLayout/common/common-header/common-header.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="toolbar common-header-wrapper">
+        <el-badge :value="200" :max="99" :offset="[-10, 5]">
+            <el-tooltip
+                effect="dark"
+                content="消息提醒"
+                placement="top-start"
+            >
+                <el-icon><Bell /></el-icon>
+            </el-tooltip>
+        </el-badge>
+        <el-tooltip
+            effect="dark"
+            content="系统设置"
+            placement="top-start"
+        >
+            <el-icon><Setting /></el-icon>
+        </el-tooltip>
+        <span class="divider"></span>
         <currentUser/>
-        <signOut/>
+        <img src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" alt="" srcset="">
     </div>
 </template>
 
@@ -12,26 +29,47 @@ import currentUser from './current-user.vue';
 </script>
 <style lang='scss'>
     .common-header-wrapper{
+        .el-badge{
+            margin-top: 4px;
+        }
+        .el-icon{
+            font-size: 20px;
+            margin: 0px 20px;
+            cursor: pointer;
+            &:hover{
+                color: #005eff;
+                transition: all .3s;
+            }
+        }
+        .divider{
+            display: block;
+            height: 15px;
+            width: 1px;
+            background: #000;
+        }
+        img{
+            border-radius: 100%;
+        }
         .common-header-item{
             margin: 0px 10px;
             display: flex;
             align-items: center;
             justify-content: center;
-            font-size: 20px;
+            font-size: 14px;
             cursor: pointer;
-            svg{
-                height: 25px;
-                width: 25px;
-            }
-            &:hover{
-                color: #409eff;
-                transition: all .2s;
-                svg{
-                    path{
-                        stroke: #409eff;
-                    }
-                }
-            }
+            // svg{
+            //     height: 25px;
+            //     width: 25px;
+            // }
+            // &:hover{
+            //     color: #409eff;
+            //     transition: all .2s;
+            //     svg{
+            //         path{
+            //             stroke: #409eff;
+            //         }
+            //     }
+            // }
         }
     }
 </style>
--- src/main/webapp/app/components/BasicLayout/common/common-header/current-user.vue
+++ src/main/webapp/app/components/BasicLayout/common/common-header/current-user.vue
@@ -1,26 +1,42 @@
 <template>
   <div class='current-user-wrapper common-header-item'>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" ><path fill="currentColor" d="M288 320a224 224 0 1 0 448 0 224 224 0 1 0-448 0m544 608H160a32 32 0 0 1-32-32v-96a160 160 0 0 1 160-160h448a160 160 0 0 1 160 160v96a32 32 0 0 1-32 32z"></path></svg>
-    <span @click="setUser">
-      {{username}}[xxx部门]
-    </span>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        {{username}} [超级管理员]
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item @click="setUser">编辑资料</el-dropdown-item>
+          <el-dropdown-item @click="logout">注销</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { inject } from 'vue'
 import {useRouter} from 'vue-router'
+import { useStore } from '@/store';
+const router = useRouter();
+const store = useStore();
+
 import useMenuTabStore from '@/store/model/menuTabs/index'
 const username = inject('currentUsername');
 
 let menuTabStore = useMenuTabStore()
 const {addMenu} = menuTabStore
 
-const router = useRouter()
 
 const setUser = ()=>{
-  router.push("/account/settings")
-  addMenu({name:'账户设置',path:"/account/settings"})
+  addMenu({path:"/account/settings",name:"Settings",title:"用户设置",icon:"User"})
+}
+
+const logout = ()=>{
+  localStorage.removeItem('jhi-authenticationToken');
+  sessionStorage.removeItem('jhi-authenticationToken');
+  store.logout();
+  router.push('/login');
 }
 
 </script>
