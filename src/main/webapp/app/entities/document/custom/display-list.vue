66cf6498bdbff85cf9a86473a88ebcf9cc846f8b
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-08-16 11:44:05 +0800

    目录树前端页面

---
+++ src/main/webapp/app/entities/document/custom/display-list.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div v-if="!selectNodeData">
+      <span>请选择目录或数据</span>
+    </div>
+    <div v-else>
+      <div class="operator">
+          <el-button type="primary">上传文档</el-button>
+          <el-button>修改文档信息</el-button>
+          <el-button type="danger">删除文档</el-button>
+      </div>
+      <el-table :data="tableData" style="width: 100%" v-loading="loading">
+        <el-table-column prop="index" label="NO" width="80">
+          <template #default="scope">
+            {{scope.$index+1}}
+          </template>
+        </el-table-column>
+        <el-table-column prop="name" label="名称"  align="center"/>
+        <el-table-column prop="createTime" label="上传时间"  align="center"/>
+        <el-table-column prop="createUserId" label="上传人" align="center"/>
+      </el-table>
+    </div>
+
+  </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import {computed, defineProps, ref} from 'vue'
+  import {type TreeNode} from './api/index'
+
+  const props = defineProps<{
+    selectNodeData: TreeNode|undefined;
+  }>();
+
+  const loading = ref(false)
+
+  const tableData = computed(()=>{
+    let data:any = []
+    loading.value = true
+    const func = (selectNodeData:TreeNode)=>{
+      if(selectNodeData.fileType=='file'){
+        data.push({
+          ...selectNodeData
+        })
+      }else{
+        if(selectNodeData.children&&selectNodeData.children.length>0){
+          selectNodeData.children.forEach(item=>{
+            func(item)
+          })
+        }
+      }
+    }
+    if(props.selectNodeData){
+      func(props.selectNodeData)
+    }
+    setTimeout(()=>{
+      loading.value = false
+    },500)
+    return data
+  })
+  </script>
+  
--- src/main/webapp/app/entities/document/document.vue
+++ src/main/webapp/app/entities/document/document.vue
@@ -1,243 +1,48 @@
 <template>
-  <div>
-    <h2 id="page-heading" data-cy="DocumentHeading">
-      <span v-text="t$('jy1App.document.home.title')" id="document-heading"></span>
-      <div class="d-flex justify-content-end">
-        <el-button class="btn btn-info mr-2" v-on:click="handleSyncList" :disabled="isFetching">
-          <font-awesome-icon icon="sync" :spin="isFetching"></font-awesome-icon>
-          <span v-text="t$('jy1App.document.home.refreshListLabel')"></span>
-        </el-button>
-        <router-link :to="{ name: 'DocumentCreate' }" custom v-slot="{ navigate }">
-          <el-button
-            @click="navigate"
-            id="jh-create-entity"
-            data-cy="entityCreateButton"
-            class="btn btn-primary jh-create-entity create-document"
-            type="primary"
-          >
-            <font-awesome-icon icon="plus"></font-awesome-icon>
-            <span v-text="t$('jy1App.document.home.createLabel')"></span>
-          </el-button>
-        </router-link>
-      </div>
-    </h2>
-    <br />
-    <div class="alert alert-warning" v-if="!isFetching && documents && documents.length === 0">
-      <span v-text="t$('jy1App.document.home.notFound')"></span>
+  <div class="document-wrapper">
+    <div class="left-tree-wrapper">
+      <LeftTree :treeData="treeData" :setCurSelectData="setCurSelectData"/>
     </div>
-    <div class="table-responsive" v-if="documents && documents.length > 0">
-      <el-table :data="documents" style="width: 100%" border stripe fit v-loading="isFetching">
-        <el-table-column min-width="150px" show-overflow-tooltip prop="id" :label="t$('global.field.id')">
-          <template #default="scope">
-            <router-link :to="{ name: 'DocumentView', params: { documentId: scope.row.id } }">{{ scope.row.id }}</router-link>
-          </template>
-        </el-table-column>
-        <el-table-column
-          min-width="150px"
-          show-overflow-tooltip
-          prop="documentname"
-          :label="t$('jy1App.document.documentname')"
-          :sortable="false"
-        >
-          <template #default="scope">
-            <span class="field-default">{{ scope.row.documentname }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          min-width="150px"
-          show-overflow-tooltip
-          prop="documenttype"
-          :label="t$('jy1App.document.documenttype')"
-          :sortable="true"
-        >
-          <template #default="scope">
-            <span class="field-default">{{ scope.row.documenttype }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          min-width="150px"
-          show-overflow-tooltip
-          prop="documentsize"
-          :label="t$('jy1App.document.documentsize')"
-          :sortable="false"
-        >
-          <template #default="scope">
-            <span class="field-default">{{ scope.row.documentsize }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          min-width="150px"
-          show-overflow-tooltip
-          prop="secretlevel"
-          :label="t$('jy1App.document.secretlevel')"
-          :sortable="false"
-        >
-          <template #default="scope">
-            <span class="field-fieldIsEnum" v-text="t$('jy1App.Secretlevel.' + scope.row.secretlevel)"></span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          min-width="150px"
-          show-overflow-tooltip
-          prop="createtime"
-          :label="t$('jy1App.document.createtime')"
-          :sortable="true"
-        >
-          <template #default="scope">
-            <span class="field-default">{{ scope.row.createtime }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          min-width="150px"
-          show-overflow-tooltip
-          prop="creatorname"
-          :label="t$('jy1App.document.creatorname')"
-          :sortable="false"
-        >
-          <template #default="scope">
-            <span class="field-default">{{ scope.row.creatorname }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="150px" show-overflow-tooltip prop="creatorid.id" :label="t$('jy1App.document.creatorid')">
-          <template #default="scope">
-            <td>
-              <div v-if="scope.row.creatorid">
-                <router-link :to="{ name: 'OfficersView', params: { officersId: scope.row.creatorid.id } }">{{
-                  scope.row.creatorid.id
-                }}</router-link>
-              </div>
-            </td>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="150px" show-overflow-tooltip prop="projectwbs.id" :label="t$('jy1App.document.projectwbs')">
-          <template #default="scope">
-            <td>
-              <div v-if="scope.row.projectwbs">
-                <router-link :to="{ name: 'ProjectwbsView', params: { projectwbsId: scope.row.projectwbs.id } }">{{
-                  scope.row.projectwbs.id
-                }}</router-link>
-              </div>
-            </td>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="150px" show-overflow-tooltip label="操作">
-          <template #default="scope">
-            <td class="text-right">
-              <div class="btn-group">
-                <router-link :to="{ name: 'DocumentView', params: { documentId: scope.row.id } }" custom v-slot="{ navigate }">
-                  <button @click="navigate" class="btn btn-info btn-sm details" data-cy="entityDetailsButton">
-                    <font-awesome-icon icon="eye"></font-awesome-icon>
-                    <span class="d-none d-md-inline" v-text="t$('entity.action.view')"></span>
-                  </button>
-                </router-link>
-                <router-link :to="{ name: 'DocumentEdit', params: { documentId: scope.row.id } }" custom v-slot="{ navigate }">
-                  <button @click="navigate" class="btn btn-primary btn-sm edit" data-cy="entityEditButton">
-                    <font-awesome-icon icon="pencil-alt"></font-awesome-icon>
-                    <span class="d-none d-md-inline" v-text="t$('entity.action.edit')"></span>
-                  </button>
-                </router-link>
-                <b-button
-                  v-on:click="prepareRemove(scope.row)"
-                  variant="danger"
-                  class="btn btn-sm"
-                  data-cy="entityDeleteButton"
-                  v-b-modal.removeEntity
-                >
-                  <font-awesome-icon icon="trash"></font-awesome-icon>
-                  <span class="d-none d-md-inline" v-text="t$('entity.action.delete')"></span>
-                </b-button>
-              </div>
-            </td>
-          </template>
-        </el-table-column>
-      </el-table>
-      <!-- <table class="table table-striped" aria-describedby="documents">
-                <thead>
-                <tr>
-                    <th scope="row"><span v-text="t$('global.field.id')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.documentname')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.documenttype')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.documentsize')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.secretlevel')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.createtime')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.creatorname')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.creatorid')"></span></th>
-                    <th scope="row"><span v-text="t$('jy1App.document.projectwbs')"></span></th>
-                    <th scope="row"></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="document in documents"
-                    :key="document.id" data-cy="entityTable">
-                    <td>
-                        <router-link :to="{name: 'DocumentView', params: {documentId: document.id}}">{{document.id}}</router-link>
-                    </td>
-                    <td>{{document.documentname}}</td>
-                    <td>{{document.documenttype}}</td>
-                    <td>{{document.documentsize}}</td>
-                    <td v-text="t$('jy1App.Secretlevel.' + document.secretlevel)"></td>
-                    <td>{{document.createtime}}</td>
-                    <td>{{document.creatorname}}</td>
-                    <td>
-                        <div v-if="document.creatorid">
-                            <router-link :to="{name: 'OfficersView', params: {officersId: document.creatorid.id}}">{{document.creatorid.id}}</router-link>
-                        </div>
-                    </td>
-                    <td>
-                        <div v-if="document.projectwbs">
-                            <router-link :to="{name: 'ProjectwbsView', params: {projectwbsId: document.projectwbs.id}}">{{document.projectwbs.id}}</router-link>
-                        </div>
-                    </td>
-                    <td class="text-right">
-                        <div class="btn-group">
-                            <router-link :to="{name: 'DocumentView', params: {documentId: document.id}}" custom v-slot="{ navigate }">
-                                <button @click="navigate" class="btn btn-info btn-sm details" data-cy="entityDetailsButton">
-                                    <font-awesome-icon icon="eye"></font-awesome-icon>
-                                    <span class="d-none d-md-inline" v-text="t$('entity.action.view')"></span>
-                                </button>
-                            </router-link>
-                            <router-link :to="{name: 'DocumentEdit', params: {documentId: document.id}}" custom v-slot="{ navigate }">
-                                <button @click="navigate" class="btn btn-primary btn-sm edit" data-cy="entityEditButton">
-                                    <font-awesome-icon icon="pencil-alt"></font-awesome-icon>
-                                    <span class="d-none d-md-inline" v-text="t$('entity.action.edit')"></span>
-                                </button>
-                            </router-link>
-                            <b-button v-on:click="prepareRemove(document)"
-                                   variant="danger"
-                                   class="btn btn-sm"
-                                   data-cy="entityDeleteButton"
-                                   v-b-modal.removeEntity>
-                                <font-awesome-icon icon="times"></font-awesome-icon>
-                                <span class="d-none d-md-inline" v-text="t$('entity.action.delete')"></span>
-                            </b-button>
-                        </div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>-->
-    </div>
-    <b-modal ref="removeEntity" id="removeEntity">
-      <template #modal-title>
-        <span id="jy1App.document.delete.question" data-cy="documentDeleteDialogHeading" v-text="t$('entity.delete.title')"></span>
-      </template>
-      <div class="modal-body">
-        <p id="jhi-delete-document-heading" v-text="t$('jy1App.document.delete.question', { id: removeId })"></p>
+    <div class="main-content">
+      <div class="display-list">
+        <DispalyList :selectNodeData="selectNodeData"/>
       </div>
-      <template #modal-footer>
-        <div>
-          <button type="button" class="btn btn-secondary" v-text="t$('entity.action.cancel')" v-on:click="closeDialog()"></button>
-          <button
-            type="button"
-            class="btn btn-primary"
-            id="jhi-confirm-delete-document"
-            data-cy="entityConfirmDeleteButton"
-            v-text="t$('entity.action.delete')"
-            v-on:click="removeDocument()"
-          ></button>
-        </div>
-      </template>
-    </b-modal>
+    </div>
   </div>
 </template>
 
-<script lang="ts" src="./document.component.ts"></script>
+<script setup lang='ts'>
+import {onMounted,ref} from 'vue'
+import LeftTree from './custom/left-tree.vue'
+import DispalyList from './custom/display-list.vue'
+import {getTreeData,type TreeNode} from './custom/api/index.ts'
+
+
+const treeData = ref<TreeNode[]>()
+const selectNodeData = ref<TreeNode >()
+
+
+onMounted(async()=>{
+  let _treeData = await getTreeData()
+  console.log('treeData',treeData)
+  treeData.value = _treeData
+})
+
+const setCurSelectData = (_selectNodeData:TreeNode)=>{
+  console.log('zhou',_selectNodeData.fileType)
+  selectNodeData.value = _selectNodeData
+}
+
+
+</script>
+<style lang='scss' scoped>
+  .document-wrapper{
+    display: flex;
+    .left-tree-wrapper{
+      flex-basis: 300px;
+    }
+    .main-content{
+      flex: 1;
+    }
+  }
+</style>
