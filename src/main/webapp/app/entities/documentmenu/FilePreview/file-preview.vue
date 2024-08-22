e6b4549e1e277ad5c810016066576f529f8a606e
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-08-22 15:27:08 +0800

    设计以数据查询+增加不同渲染器的方式来实现文件预览，当前完成了txt文件的预览，png的预览（接口返回的数据好像有点问题）后续其他格式只需新增渲染器即可

---
+++ src/main/webapp/app/entities/documentmenu/FilePreview/file-preview.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="file-preview-wrapper" v-loading="loading">
+        <pre>
+            <component :is="map[fileType]" :fileInfo="fileInfo"/>
+        </pre>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import type { IDocumentmenu } from '@/shared/model/documentmenu.model';
+import axios from 'axios';
+import { ref, reactive, onMounted, inject, computed, defineComponent, type Ref} from 'vue'
+import TxtViewer from './previewer/txt-viewer.vue';
+import pngViewer from './previewer/png-viewer.vue'; 
+
+    const map:Record<string,any> = {
+        "txt":TxtViewer,
+        "png":pngViewer
+    }
+
+
+    const loading = ref(true)
+    const curSelectFile = inject<Ref<IDocumentmenu>>('curSelectFile')
+    const fileType = computed(()=>{
+        let fileName = curSelectFile?.value?.menuname
+        return fileName?.split('.').pop()||''
+    })
+    // 根据选中的文件查询到的文件内容
+    const fileInfo = ref()
+    onMounted(async()=>{
+        const fileurl = curSelectFile?.value.fileurl ?? '';
+        const filename = curSelectFile?.value.menuname ?? '';
+        const baseApiUrl = 'api/files/download';
+        let _fileInfo = await axios.get(baseApiUrl, {
+            responseType: 'arraybuffer',
+            params:{fileurl: fileurl} 
+        })
+        fileInfo.value = _fileInfo.data
+        loading.value = false
+    })
+
+</script>
+<style lang='scss' scoped>
+
+</style>
---
+++ src/main/webapp/app/entities/documentmenu/FilePreview/previewer/png-viewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- {{fileInfo}} -->
+    <img :src="url" alt="" srcset="">
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive} from 'vue'
+    const props = defineProps({
+        fileInfo:String as any
+    })
+
+    const url = ref(window.URL.createObjectURL(new Blob([props.fileInfo])));
+	console.log(url)
+</script>
+<style lang='scss' scoped>
+
+</style>
