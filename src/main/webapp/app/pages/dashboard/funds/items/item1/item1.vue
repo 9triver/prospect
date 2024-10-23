88d3c1e20467a36d4bd96d588b22c8fe496577c4
DaTou <1838825829@qq.com>
2024-10-23 21:29:14 +0800

    经费看板初版

---
+++ src/main/webapp/app/pages/dashboard/funds/items/item1/item1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="chart-content" ref="chart-content"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+
+export default {
+  name: 'funds-basic',
+  data() {
+    return {}
+  },
+  props: {},
+  methods: {},
+  components: {
+
+  },
+  mounted() {
+    const chart = echarts.init(this.$refs['chart-content'])
+    chart.setOption({
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: '总计' },
+            { value: 735, name: '已花费' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    })
+  }
+}
+
+</script>
+
+<style>
+#chart-content {
+  height: 100%;
+  width: 100%;
+}
+</style>
