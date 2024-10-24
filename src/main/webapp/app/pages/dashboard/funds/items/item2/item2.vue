b9ad32a7230a2932a67b73c847e1864989b8ab63
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-10-24 20:45:16 +0800

    异常情况独立在图表中标识出来

--- src/main/webapp/app/pages/dashboard/funds/items/item2/item2.vue
+++ src/main/webapp/app/pages/dashboard/funds/items/item2/item2.vue
@@ -9,7 +9,7 @@
 
 <script>
 import * as echarts from "echarts";
-import {getDataSource} from './api/index.js'
+import { getDataSource } from './api/index.js'
 
 export default {
   name: 'funds-basic',
@@ -31,6 +31,59 @@ export default {
         shadowColor: 'rgba(0,0,0,0.3)'
       }
     };
+    const generateDataAndMarkPoint = () => {
+      let data = []
+      let markPoint = []
+      dataSource.series2.data.forEach((item, index) => {
+        let total = dataSource.series1.data[index]
+        let color = "#31395d"
+        if (item > total) {
+          color = "#ff4d4f"
+          markPoint.push({
+            coord: [index, item], // 使用坐标定义位置
+            name: '超出预算',
+            symbol: 'pin', // 标记符号类型
+            symbolSize: 20, // 标记符号大小
+            label: {
+              show: true,
+              position: 'right',
+              formatter: '超出预算'
+            },
+            itemStyle: {
+              color: color
+            },
+          })
+        }
+        if (item > total*0.75&& item <= total) {
+          markPoint.push({
+            coord: [index, total], // 使用坐标定义位置
+            name: '预算不足25%',
+            symbol: 'pin', // 标记符号类型
+            symbolSize: 20, // 标记符号大小
+            label: {
+              show: true,
+              position: 'right',
+              formatter: '预算不足25%'
+            },
+            itemStyle: {
+              color: color
+            },
+          })
+        }
+        data.push({
+          value: item,
+          itemStyle: {
+            color: color
+          }
+        })
+      })
+      return {
+        data,
+        markPoint
+      }
+    }
+    let { data, markPoint } = generateDataAndMarkPoint()
+
     let option = {
       legend: {},
       tooltip: {
@@ -44,7 +97,7 @@ export default {
         splitArea: { show: false }
       },
       yAxis: {
-        name:"金额（万元）"
+        name: "金额（万元）"
       },
       grid: {
         bottom: 30
@@ -55,12 +108,12 @@ export default {
           type: 'bar',
           emphasis: emphasisStyle,
           data: dataSource.series1.data,
-          barGap:"-100%",
-          barWidth:"20%",
+          barGap: "-100%",
+          barWidth: "20%",
           itemStyle: {
             normal: {
               color: '#5470c6',
-              borderRadius:10
+              borderRadius: 10
             }
           }
         },
@@ -68,13 +121,16 @@ export default {
           name: dataSource.series2.name,
           type: 'bar',
           emphasis: emphasisStyle,
-          data: dataSource.series2.data,
-          barWidth:"20%",
+          data: data,
+          barWidth: "20%",
           itemStyle: {
             normal: {
               color: '#31395d',
-              borderRadius:10
+              borderRadius: 10
             }
+          },
+          markPoint: {
+            data: markPoint
           }
         }
       ]
@@ -93,6 +149,7 @@ export default {
   display: flex;
   flex-direction: column;
 }
+
 #chart-content .title {
   font-size: 20px;
   font-weight: bold;
--- src/main/webapp/app/pages/dashboard/funds/items/item3/item3.vue
+++ src/main/webapp/app/pages/dashboard/funds/items/item3/item3.vue
@@ -9,7 +9,7 @@
 
 <script>
 import * as echarts from "echarts";
-import {getDataSource} from './api/index.js'
+import { getDataSource } from './api/index.js'
 
 export default {
   name: 'funds-basic',
@@ -33,13 +33,15 @@ export default {
     };
     let option = {
       legend: {
-        data:[
+        data: [
           {
             name: dataSource.series1.name,
             icon: 'rect',
-          },{
+          }, {
             name: dataSource.series2.name,
             icon: 'rect',
+          }, {
+            name: "支出占比"
           }
         ]
       },
@@ -53,9 +55,20 @@ export default {
         splitLine: { show: false },
         splitArea: { show: false }
       },
-      yAxis: {
-        name:"金额（千万）"
-      },
+      yAxis: [
+        {
+          name: "金额（千万）",
+          splitLine: {
+            show: false
+          }
+        },
+        {
+          name: "占比（%）",
+          splitLine: {
+            show: false
+          }
+        }
+      ],
       grid: {
         bottom: 30
       },
@@ -65,8 +78,8 @@ export default {
           type: 'bar',
           emphasis: emphasisStyle,
           data: dataSource.series1.data,
-          barGap:"-100%",
-          barWidth:"60%",
+          barGap: "-100%",
+          barWidth: "60%",
           itemStyle: {
             normal: {
               color: '#dddddd',
@@ -74,45 +87,72 @@ export default {
           }
         },
         {
-            name: dataSource.series2.name,
-            barMinHeight: 10,
-            type: "pictorialBar",
-            barCategoryGap: "60%",
-            barWidth:"60%",
-            // symbol: 'path://M0,10 L10,10 L5,0 L0,10 z',
-            symbol: "path://M0,10 L10,10 C5.5,10 5.5,5 5,0 C4.5,5 4.5,10 0,10 z",
-            itemStyle: {
-                normal: {
-                    //barBorderRadius: 5,
-                    //渐变色
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                            offset: 0,
-                            color: "#01EAED"
-                        },
-                        {
-                            offset: 0.5,
-                            color: "#02C4DD"
-                        },
-                        {
-                            offset: 1,
-                            color: "#029ED9"
-                        }
-                    ])
-                }
-            },
-            label: {
-                normal: {
-                    show: false,
-                    position: "top",
-                    textStyle: {
-                        // color: "#fff",
-                        fontSize:20
-                    }
-                }
-            },
-            data: dataSource.series2.data,
-            z: 10
+          name: dataSource.series2.name,
+          barMinHeight: 10,
+          type: "pictorialBar",
+          barCategoryGap: "60%",
+          barWidth: "60%",
+          // symbol: 'path://M0,10 L10,10 L5,0 L0,10 z',
+          symbol: "path://M0,10 L10,10 C5.5,10 5.5,5 5,0 C4.5,5 4.5,10 0,10 z",
+          itemStyle: {
+            normal: {
+              //barBorderRadius: 5,
+              //渐变色
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                offset: 0,
+                color: "#01EAED"
+              },
+              {
+                offset: 0.5,
+                color: "#02C4DD"
+              },
+              {
+                offset: 1,
+                color: "#029ED9"
+              }
+              ])
+            }
+          },
+          label: {
+            normal: {
+              show: false,
+              position: "top",
+              textStyle: {
+                // color: "#fff",
+                fontSize: 20
+              }
+            }
+          },
+          data: dataSource.series2.data,
+          z: 10
         },
+        {
+          name: "支出占比",
+          type: 'line',
+          data: dataSource.series2.data.map((item, index) => item / dataSource.series1.data[index] * 100),
+          smooth: true,
+          symbol: 'none',
+          symbolSize: 0,
+          yAxisIndex: 1,
+          symbol: 'circle',
+          itemStyle: {
+            color: '#FF0000'
+          },
+          lineStyle: {
+            color: '#ffcaca'
+          },
+          markLine: {
+            // data: [{
+            //     yAxis: 80
+            // }]
+            data: [
+              [{ coord: [0, 80] }, { coord: [dataSource.xAxis.data.length - 1, 80] }]  // 自定义线段
+            ]
+          }
+          // areaStyle: {
+          // color: '#FF0000'
+          // }
+        }
       ]
     };
     chart.setOption(option);
@@ -129,6 +169,7 @@ export default {
   display: flex;
   flex-direction: column;
 }
+
 #chart-content .title {
   font-size: 20px;
   font-weight: bold;
