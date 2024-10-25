b817b142cf651d99d9519dd9ec580eea22d422d8
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-10-25 14:56:28 +0800

    新增图表之间的联动

--- src/main/webapp/app/pages/dashboard/funds/items/item2/api/index.js
+++ src/main/webapp/app/pages/dashboard/funds/items/item2/api/index.js
@@ -1,21 +1,75 @@
 export async function getDataSource() {
-    let xAxisData = [];
-    for (let i = 0; i < 10; i++) {
-        xAxisData.push('承研合同' + (i + 1));
-    }
-    let data = {
-        "xAxis": {
-            name: "承研合同",
-            data: xAxisData
+    let dataSource = [
+        {
+            "name":"承研合同1",
+            "total":100,    //整体金额
+            "pay":80,        //支出金额
+            "cost":10,       //成本
+            "apply":3,      //实施
         },
-        "series1": {
-            name: "整体金额",
-            data: [100, 100, 34, 14, 25, 35, 25, 54, 32, 65]
+        {
+            "name":"承研合同2",
+            "total":100,
+            "pay":110,
+            "cost":4,
+            "apply":5,
         },
-        "series2": {
-            name: "支出金额",
-            data: [9, 110, 12, 11, 12, 13, 14, 15, 28, 17]
+        {
+            "name":"承研合同3",
+            "total":34,
+            "pay":12,
+            "cost":6,
+            "apply":7,
         },
-    }
-    return data;
+        {
+            "name":"承研合同4",
+            "total":14,
+            "pay":12,
+            "cost":8,
+            "apply":9,
+        },
+        {
+            "name":"承研合同5",
+            "total":25,
+            "pay":12,
+            "cost":10,
+            "apply":11,
+        },
+        {
+            "name":"承研合同6",
+            "total":35,
+            "pay":13,
+            "cost":12,
+            "apply":13,
+        },
+        {
+            "name":"承研合同7",
+            "total":25,
+            "pay":14,
+            "cost":14,
+            "apply":15,
+        },
+        {
+            "name":"承研合同8",
+            "total":54,
+            "pay":15,
+            "cost":16,
+            "apply":17,
+        },
+        {
+            "name":"承研合同9",
+            "total":32,
+            "pay":28,
+            "cost":18,
+            "apply":19,
+        },
+        {
+            "name":"承研合同10",
+            "total":65,
+            "pay":17,
+            "cost":20,
+            "apply":21,
+        }
+    ]
+    return dataSource;
 }
