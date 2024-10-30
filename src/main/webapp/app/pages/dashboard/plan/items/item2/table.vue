d2d0668307a90c927720933a89f91c3e0bebf5a1
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-10-30 11:06:23 +0800

    wbs列表展示内容修改

--- src/main/webapp/app/pages/dashboard/plan/items/item2/table.vue
+++ src/main/webapp/app/pages/dashboard/plan/items/item2/table.vue
@@ -1,78 +1,73 @@
 <template>
-    <div>
-      <el-table
-        :data="tableData"
-        style="width: 100%;margin-bottom: 20px;"
-        row-key="id"
-        border
-        default-expand-all
-        :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-        <el-table-column
-          prop="projectName"
-          label="项目"
-          sortable
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="budgetAmount"
-          label="预算金额"
-          sortable
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="estimateAmount"
-          label="估算金额">
-        </el-table-column>
-        <el-table-column
-          prop="actualPaymentAmount"
-          label="实际支付金额">
-        </el-table-column>
-      </el-table>
-    </div>
-    </template>
-    <script>
-      export default {
-        data() {
-          return {
-            tableData: [{
-              id: 1,
-              projectName: '产品1',
-              budgetAmount: '100',
-              estimateAmount: '100',
-              actualPaymentAmount: '200'
-            }, {
-              id: 2,
-              projectName: '产品2',
-              budgetAmount: '100',
-              estimateAmount: '200',
-              actualPaymentAmount: '200'
-            }, {
-              id: 3,
-              projectName: '产品3',
-              budgetAmount: '100',
-              estimateAmount: '200',
-              actualPaymentAmount:200,
-              children: [{
-                  id: 31,
-                  projectName: '子产品3-1',
-                  budgetAmount: '100',
-                  estimateAmount: '200',
-                  actualPaymentAmount: '200'
-                }, {
-                  id: 32,
-                  projectName: '子产品3-2',
-                  budgetAmount: '100',
-                  estimateAmount: '200',
-                  actualPaymentAmount: '200'
-              }]
-            }, {
-              id: 4,
-              projectName: '产品4',
-              budgetAmount: '100',
-              estimateAmount: '200',
-              actualPaymentAmount: '200'
-            }]
-          }
+  <div>
+    <el-table :data="tableData" style="width: 100%;margin-bottom: 20px;" row-key="id" border default-expand-all
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }">
+      <el-table-column prop="projectName" label="任务名称" sortable width="180">
+      </el-table-column>
+      <el-table-column prop="actualStartTime" label="实际开始时间" sortable width="180" align="center">
+      </el-table-column>
+      <el-table-column prop="actualEndTime" label="实际结束时间" align="center">
+      </el-table-column>
+      <el-table-column label="进度" align="center">
+        <template #default="scope">
+          <el-progress :percentage="scope.row.process" :format="formatProgress"/>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+export default {
+
+  data() {
+    return {
+      tableData: [
+        {
+          "id": 1,
+          "projectName": "产品1",
+          "actualStartTime": "2023-01-01",
+          "actualEndTime": "-",
+          "process": 24
+        },
+        {
+          "id": 2,
+          "projectName": "产品2",
+          "actualStartTime": "2023-02-01",
+          "actualEndTime": "2023-02-28",
+          "process": 100
+        },
+        {
+          "id": 3,
+          "projectName": "产品3",
+          "actualStartTime": "2023-03-01",
+          "actualEndTime": "-",
+          "process": 62,
+          "children": [
+            {
+              "id": 31,
+              "projectName": "子产品3-1",
+              "actualStartTime": "2023-04-01",
+              "actualEndTime": "-",
+              "process": 7
+            },
+            {
+              "id": 32,
+              "projectName": "子产品3-2",
+              "actualStartTime": "2023-05-01",
+              "actualEndTime": "-",
+              "process": 6
+            }
+          ]
+        },
+        {
+          "id": 4,
+          "projectName": "产品4",
+          "actualStartTime": "2023-06-01",
+          "actualEndTime": "-",
+          "process": 79
         }
-      }
-    </script>
+      ]
+    }
+  }
+}
+</script>
