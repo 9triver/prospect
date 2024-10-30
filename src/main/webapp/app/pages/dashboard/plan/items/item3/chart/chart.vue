4b599d2591807e2960d6f2b397a6f1af3fe579fa
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-10-30 11:06:43 +0800

    任务包详情增加区域缩放组件

--- src/main/webapp/app/pages/dashboard/plan/items/item3/chart/chart.vue
+++ src/main/webapp/app/pages/dashboard/plan/items/item3/chart/chart.vue
@@ -14,14 +14,14 @@ export default {
   props: {},
   methods: {
     // 生成渐变色
-    generateGradientColor(color,reverse) {
+    generateGradientColor(color, reverse) {
       return new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-        offset: reverse?1:0,
+        offset: reverse ? 1 : 0,
         color: color
       },
       {
-        offset: reverse?0:1,
-        color: color
+        offset: reverse ? 0 : 1,
+        color: color + "50"
       }
       ])
     }
@@ -54,7 +54,7 @@ export default {
         show: true
       },
       xAxis: {
-        data: ['任务包1', '任务包2', '任务包3', '任务包4', '任务包5', '任务包6', '任务包7', '任务包8'],
+        data: ['任务包1', '任务包2', '任务包3', '任务包4', '任务包5', '任务包6', '任务包7', '任务包8', '任务包9', '任务包10'],
         axisLine: {
           show: true, //隐藏X轴轴线
           lineStyle: {
@@ -112,14 +112,14 @@ export default {
         {
           name: "计划进度",
           type: "bar",
-          barWidth: 20,
+          barWidth: 10,
           itemStyle: {
             normal: {
               color: this.generateGradientColor("#5470c6"),
-              borderRadius:5
+              barBorderRadius: [30, 30, 0, 0]
             }
           },
-          data: [20, 80, 100, 40, 34, 90, 60, 60],
+          data: [20, 80, 100, 40, 34, 90, 60, 60,100, 80],
           z: 10,
           zlevel: 0,
           "label": {
@@ -130,41 +130,41 @@ export default {
             "color": "#01fff4"
           }
         },
-        {
-          // 分隔
-          type: "pictorialBar",
-          itemStyle: {
-            normal: {
-              color: "#fff"
-            }
-          },
-          symbolRepeat: "fixed",
-          symbolMargin: 6,
-          symbol: "rect",
-          symbolClip: true,
-          symbolSize: [20, 1],
-          symbolPosition: "start",
-          symbolOffset: [-24, 0],
-          // symbolBoundingData: this.total,
-          data: [20, 80, 100, 40, 34, 90, 60, 60],
-          width: 25,
-          z: 0,
-          tooltip: {
-            show: false
-          },
-          zlevel: 1,
-        },
+        // {
+        //   // 分隔
+        //   type: "pictorialBar",
+        //   itemStyle: {
+        //     normal: {
+        //       color: "#fff"
+        //     }
+        //   },
+        //   symbolRepeat: "fixed",
+        //   symbolMargin: 6,
+        //   symbol: "rect",
+        //   symbolClip: true,
+        //   symbolSize: [20, 1],
+        //   symbolPosition: "start",
+        //   symbolOffset: [-24, 0],
+        //   // symbolBoundingData: this.total,
+        //   data: [20, 80, 100, 40, 34, 90, 60, 60],
+        //   width: 25,
+        //   z: 0,
+        //   tooltip: {
+        //     show: false
+        //   },
+        //   zlevel: 1,
+        // },
         {
           name: "实际进度",
           type: "bar",
-          barWidth: 20,
+          barWidth: 10,
           itemStyle: {
             normal: {
               color: this.generateGradientColor("#31395d"),
-              borderRadius:5
+              barBorderRadius: [30, 30, 0, 0]
             }
           },
-          data: [20, 80, 100, 40, 34, 90, 60, 60],
+          data: [20, 80, 100, 40, 34, 90, 60, 60, 20, 80],
           z: 10,
           zlevel: 0,
           "label": {
@@ -175,68 +175,86 @@ export default {
             "color": "#01fff4"
           }
         },
+        // {
+        //   // 分隔
+        //   type: "pictorialBar",
+        //   itemStyle: {
+        //     normal: {
+        //       color: "#fff"
+        //     }
+        //   },
+        //   symbolRepeat: "fixed",
+        //   symbolMargin: 6,
+        //   symbol: "rect",
+        //   symbolClip: true,
+        //   symbolSize: [20, 1],
+        //   symbolPosition: "start",
+        //   symbolOffset: [0, -1],
+        //   // symbolBoundingData: this.total,
+        //   data: [20, 80, 100, 40, 34, 90, 60, 60],
+        //   width: 25,
+        //   z: 0,
+        //   tooltip: {
+        //     show: false
+        //   },
+        //   zlevel: 1,
+        // },
         {
-          // 分隔
-          type: "pictorialBar",
-          itemStyle: {
-            normal: {
-              color: "#fff"
-            }
-          },
-          symbolRepeat: "fixed",
-          symbolMargin: 6,
-          symbol: "rect",
-          symbolClip: true,
-          symbolSize: [20, 1],
-          symbolPosition: "start",
-          symbolOffset: [0, -1],
-          // symbolBoundingData: this.total,
-          data: [20, 80, 100, 40, 34, 90, 60, 60],
-          width: 25,
-          z: 0,
-          tooltip: {
-            show: false
-          },
-          zlevel: 1,
-        },
-        {
-          name: "差值",
+          name: "进度偏差",
           type: "bar",
-          barWidth: 20,
-          data: [1, 2, 10, -4, -10, 10, 20, 60],
+          barWidth: 10,
+          data: [1, 2, 10, -4, -10, 10, 20, 60, -80, 80].map(item => {
+            return {
+              value: item,
+              itemStyle: {
+                color: item >= 0 ? this.generateGradientColor("#74DA81") : this.generateGradientColor("#FF0000", true),
+                barBorderRadius: item >= 0 ? [30, 30, 0, 0] : [0, 0, 30, 30]
+              }
+            }
+          }),
           itemStyle: {
-            color: (params)=> {
-              return params.data >= 0 ? this.generateGradientColor("#74DA81")  : this.generateGradientColor("#FF0000",true);
+            color: (params) => {
+              return params.data >= 0 ? this.generateGradientColor("#74DA81") : this.generateGradientColor("#FF0000", true);
             },
-            borderRadius:5
+            barBorderRadius: [30, 30, 0, 0]
           }
         },
-        {
-          // 分隔
-          type: "pictorialBar",
-          itemStyle: {
-            normal: {
-              color: "#fff"
-            }
-          },
-          symbolRepeat: "fixed",
-          symbolMargin: 6,
-          symbol: "rect",
-          symbolClip: true,
-          symbolSize: [20, 1],
-          symbolPosition: "start",
-          symbolOffset: [24, -1],
-          // symbolBoundingData: this.total,
-          data: [1, 2, 10, -4, -10, 10, 20, 60],
-          width: 25,
-          z: 0,
-          tooltip: {
-            show: false
-          },
-          zlevel: 1,
-        },
+        // {
+        //   // 分隔
+        //   type: "pictorialBar",
+        //   itemStyle: {
+        //     normal: {
+        //       color: "#fff"
+        //     }
+        //   },
+        //   symbolRepeat: "fixed",
+        //   symbolMargin: 6,
+        //   symbol: "rect",
+        //   symbolClip: true,
+        //   symbolSize: [20, 1],
+        //   symbolPosition: "start",
+        //   symbolOffset: [24, -1],
+        //   // symbolBoundingData: this.total,
+        //   data: [1, 2, 10, -4, -10, 10, 20, 60],
+        //   width: 25,
+        //   z: 0,
+        //   tooltip: {
+        //     show: false
+        //   },
+        //   zlevel: 1,
+        // },
 
-      ]
+      ],
+      "dataZoom": [{
+        "show": true,
+        "height": 30,
+        "xAxisIndex": [
+          0
+        ],
+        bottom: 0,
+        "start": 10,
+        "end": 80
+      }],
     };
     chart.setOption(option);
     window.addEventListener('resize', function () {
