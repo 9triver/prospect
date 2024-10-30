8dc0c89c08fb79c14c48184df883ba553d7f8778
zhou_hmeng <zhou_hmeng@cvicse.com>
2024-10-30 09:08:50 +0800

    计划看板初版

--- src/main/webapp/app/pages/dashboard/plan/items/item2/item.vue
+++ src/main/webapp/app/pages/dashboard/plan/items/item2/item.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>11</div>
+    <div class="item-content-wrapper">
+        <div class="title">WBS列表</div>
+        <div class="table-content">
+            <MyTable/>
+        </div>
+    </div>
 </template>
 <script>
+    import MyTable from './table.vue'
     export default{
+       name:'myComponent',
        data(){
            return{}
        },
        props:{},
        methods:{},
-       components:{},
+       components:{
+        MyTable
+       },
 }
 </script>
-<style>
+<style scoped>
+    .item-content-wrapper{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        overflow-y: scroll;
+    }
+    .item-content-wrapper .title{
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .item-content-wrapper .table-content{
+        float: 1;
+    }
+
 </style>
